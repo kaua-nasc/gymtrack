c18b8ff8820632472be8e05a3f5d4cf5d65f3507
Kauã Alves <kauaalvesnascimento22@gmail.com>
2025-09-14 22:21:10 -0300

    test: remove duplicates from code

--- src/module/identity/__test__/e2e/auth/authentication.spec.ts
+++ src/module/identity/__test__/e2e/auth/authentication.spec.ts
@@ -5,8 +5,8 @@ import { Tables } from '@testInfra/enum/table.enum';
 import { testDbClient } from '@testInfra/knex.database';
 import { testCacheClient } from '@testInfra/test-cache.setup';
 import { createNestApp } from '@testInfra/test-e2e.setup';
-import { hash } from 'bcrypt';
 import request from 'supertest';
+import { userFactory } from '../../factory/user.factory';
 
 describe('Auth Controller (e2e)', () => {
   let app: INestApplication;
@@ -33,37 +33,25 @@ describe('Auth Controller (e2e)', () => {
 
   describe('Authentication - signIn', () => {
     it('should do login and return token', async () => {
-      const createUserInput = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-        id: '29b8c580-3224-4094-b8d5-267b5bf15eab',
-      };
+      const user = userFactory.build();
 
-      await request(app.getHttpServer()).post('/identity/user').send(createUserInput);
+      await request(app.getHttpServer()).post('/identity/user').send(user);
 
       const res = await request(app.getHttpServer()).post('/identity/auth').send({
-        email: createUserInput.email,
-        password: createUserInput.password,
+        email: user.email,
+        password: user.password,
       });
 
       expect(res.status).toBe(HttpStatus.OK);
     });
 
     it('should throws an exception and return status code unauthorized', async () => {
-      const createUserInput = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-        id: '29b8c580-3224-4094-b8d5-267b5bf15eab',
-      };
+      const user = userFactory.build();
 
-      await request(app.getHttpServer()).post('/identity/user').send(createUserInput);
+      await request(app.getHttpServer()).post('/identity/user').send(user);
 
       const res = await request(app.getHttpServer()).post('/identity/auth').send({
-        email: createUserInput.email,
+        email: user.email,
         password: '12345678',
       });
 
@@ -73,31 +61,19 @@ describe('Auth Controller (e2e)', () => {
 
   describe.skip('Authentication - Request Reset Password', () => {
     it('should return ok when request an reset password with valid email', async () => {
-      const createUserInput = {
-        id: '5e2a62de-6ead-4678-a12f-8c17e91513a3',
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-      };
-      await testDbClient(Tables.User).insert(createUserInput);
+      const user = userFactory.build();
+      await testDbClient(Tables.User).insert(user);
 
       const res = await request(app.getHttpServer())
         .post('/identity/auth/reset-password/request')
-        .send({ email: createUserInput.email });
+        .send({ email: user.email });
 
       expect(res.status).toBe(HttpStatus.OK);
     });
 
     it('should return not found status code when request reset password with any user with email', async () => {
-      const createUserInput = {
-        id: '5e2a62de-6ead-4678-a12f-8c17e91513a3',
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-      };
-      await testDbClient(Tables.User).insert(createUserInput);
+      const user = userFactory.build();
+      await testDbClient(Tables.User).insert(user);
 
       const res = await request(app.getHttpServer())
         .post('/identity/auth/reset-password/request')
@@ -124,21 +100,15 @@ describe('Auth Controller (e2e)', () => {
   });
   describe('Authentication - Verify Reset Password', () => {
     it('should return ok when user exists and is valid', async () => {
-      const createUserInput = {
-        id: '5e2a62de-6ead-4678-a12f-8c17e91513a3',
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-      };
+      const user = userFactory.build();
       const token = '1234';
 
-      await testDbClient(Tables.User).insert(createUserInput);
-      await testCacheClient.set(createUserInput.email, token);
+      await testDbClient(Tables.User).insert(user);
+      await testCacheClient.set(user.email!, token);
 
       const res = await request(app.getHttpServer())
         .post('/identity/auth/reset-password/verify')
-        .send({ email: createUserInput.email, token: token });
+        .send({ email: user.email, token: token });
 
       expect(res.status).toBe(HttpStatus.OK);
     });
@@ -150,61 +120,41 @@ describe('Auth Controller (e2e)', () => {
       expect(res.status).toBe(HttpStatus.NOT_FOUND);
     });
     it('should return bad request when token doest match', async () => {
-      const createUserInput = {
-        id: '5e2a62de-6ead-4678-a12f-8c17e91513a3',
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: '4ac66c29218da4f4a0aa4f4a43037c48936df7c9bedcae1af6ac67d8768f3c37',
-      };
+      const user = userFactory.build();
       const token = '1111';
 
-      await testDbClient(Tables.User).insert(createUserInput);
-      await testCacheClient.set(createUserInput.email, token);
+      await testDbClient(Tables.User).insert(user);
+      await testCacheClient.set(user.email!, token);
 
       const res = await request(app.getHttpServer())
         .post('/identity/auth/reset-password/verify')
-        .send({ userId: createUserInput.id, token: '1234' });
+        .send({ userId: user.id, token: '1234' });
 
       expect(res.status).toBe(HttpStatus.BAD_REQUEST);
     });
   });
   describe('Authentication - Reset Password Insert New Password', () => {
     it('should update password when user exists and new password is valid', async () => {
-      const createUserInput = {
-        id: '5e2a62de-6ead-4678-a12f-8c17e91513a3',
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: '4ac66c29218da4f4a0aa4f4a43037c48936df7c9bedcae1af6ac67d8768f3c37',
-      };
+      const user = userFactory.build();
 
-      await testDbClient(Tables.User).insert(createUserInput);
+      await testDbClient(Tables.User).insert(user);
 
       const res = await request(app.getHttpServer())
         .post('/identity/auth/reset-password/create')
-        .send({ userId: createUserInput.id, newPassword: 'password12345' });
+        .send({ userId: user.id, newPassword: 'password12345' });
 
       expect(res.status).toBe(HttpStatus.CREATED);
     });
 
     it('should return unauthorized when new password is equals to last password', async () => {
       const plainPassword = 'password123';
-      const hashedPassword = await hash(plainPassword, 10);
+      const user = userFactory.build();
 
-      const createUserInput = {
-        id: '5e2a62de-6ead-4678-a12f-8c17e91513a3',
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: hashedPassword,
-      };
-
-      await testDbClient(Tables.User).insert(createUserInput);
+      await testDbClient(Tables.User).insert(user);
 
       const res = await request(app.getHttpServer())
         .post('/identity/auth/reset-password/create')
-        .send({ userId: createUserInput.id, newPassword: plainPassword });
+        .send({ userId: user.id, newPassword: plainPassword });
 
       expect(res.status).toBe(HttpStatus.UNAUTHORIZED);
     });
--- src/module/identity/__test__/e2e/user-management/user-management.spec.ts
+++ src/module/identity/__test__/e2e/user-management/user-management.spec.ts
@@ -5,6 +5,7 @@ import { Tables } from '@testInfra/enum/table.enum';
 import { testDbClient } from '@testInfra/knex.database';
 import { createNestApp } from '@testInfra/test-e2e.setup';
 import request from 'supertest';
+import { createUserFactory, userFactory } from '../../factory/user.factory';
 
 describe('Identity - User Management Controller - (e2e)', () => {
   let app: INestApplication;
@@ -29,37 +30,26 @@ describe('Identity - User Management Controller - (e2e)', () => {
 
   describe('Create User', () => {
     it('should creates a new user', async () => {
-      const createUserInput = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-      };
+      const user = createUserFactory.build();
 
       const response = await request(app.getHttpServer())
         .post('/identity/user')
-        .send(createUserInput);
+        .send(user);
 
       expect(response.status).toBe(HttpStatus.CREATED);
     });
     it('should throws error by already used email', async () => {
-      const createUserInput = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-      };
+      const user = createUserFactory.build();
 
       await testDbClient(Tables.User).insert({
-        ...createUserInput,
+        ...user,
         id: '5e2a62de-6ead-4678-a12f-8c17e91513a3',
       });
 
       const res = await request(app.getHttpServer())
         .post('/identity/user')
         .send({
-          ...createUserInput,
-          firstName: 'Joao',
+          ...user,
         });
 
       expect(res.status).toBe(HttpStatus.CONFLICT);
@@ -68,23 +58,15 @@ describe('Identity - User Management Controller - (e2e)', () => {
 
   describe('Get User by Id', () => {
     it('should gets an user', async () => {
-      const createUserInput = {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'johndoe@example.com',
-        password: 'password123',
-        id: '5e2a62de-6ead-4678-a12f-8c17e91513a3',
-      };
+      const user = userFactory.build();
 
-      await request(app.getHttpServer()).post('/identity/user').send(createUserInput);
+      await request(app.getHttpServer()).post('/identity/user').send(user);
 
-      const res = await request(app.getHttpServer()).get(
-        '/identity/user/' + createUserInput.id
-      );
+      const res = await request(app.getHttpServer()).get('/identity/user/' + user.id);
 
       expect(res.status).toBe(HttpStatus.OK);
-      expect(res.body.id).toBe(createUserInput.id);
-      expect(res.body.email).toBe(createUserInput.email);
+      expect(res.body.id).toBe(user.id);
+      expect(res.body.email).toBe(user.email);
     });
 
     it('should return not found', async () => {
